2505c87fa1fbd6717563bf383e79ac2a0722975e
dagamed131 <104123953+dagamed131@users.noreply.github.com>
2022-10-05 13:30:41 +0600

    Create Developer.java

---
+++ Developer.java
@@ -0,0 +1,10 @@
+public class Developer {
+    Common common;
+    public void setCommon(Common common){
+        this.common = common;
+    }
+
+    public void doCommon(){
+        common.count();
+    }
+}
